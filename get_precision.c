69934d07d919cd8bf209f96757f8a7763ee3d03c
dinseid <dinzn@yahoo.com>
2023-01-24 03:48:40 -0800

    updated 10th task

--- get_precision.c
+++ get_precision.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
@@ -30,10 +31,13 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 		}
 		else
+
 			break;
+
 	}
 
 	*i = curr_i - 1;
 
 	return (precision);
+
 }
